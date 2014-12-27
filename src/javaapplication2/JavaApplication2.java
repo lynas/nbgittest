afc5ad4fd6adf19eaf8e16021ee25d5c353b9134
lynas <szlynas@gmai.com>
2014-12-27 22:51:40 +0600

    Revert "first commit 22"

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,11 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         //cvbcvb
+
+        //testing modification
+
     }
     
 }
